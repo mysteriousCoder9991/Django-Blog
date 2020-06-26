daa47d0f8f664c9514d195c2ad3f1838fb14e558
Rahul Dhar <rdhar9991@gmail.com>
2020-06-26 18:44:50 +0530

    Second Commit

--- blog/models.py
+++ blog/models.py
@@ -5,8 +5,8 @@ class Blog(models.Model):
     sno=models.AutoField(primary_key=True)
     title=models.CharField(max_length=200)
     content=models.TextField()
-    short_desc=models.CharField(max_length=300)
-    slug=models.CharField(max_length=100)
+    short_desc=models.CharField(max_length=150)
+    slug=models.CharField(max_length=50)
     time=models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
